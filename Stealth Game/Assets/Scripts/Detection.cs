7d46588507b2fa55e032ae6627b3779f4235cce7
unknown <julianrwalston@gmail.com>
2021-03-23 12:30:12 -0700

    created object prefabs and set up detection script

--- Stealth Game/Assets/Scripts/Detection.cs
+++ Stealth Game/Assets/Scripts/Detection.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Detection : MonoBehaviour
 {
+    public Transform player; // The player's position
+    public Transform guard; // The current position of the guard
+    public int sightDist;
     // Start is called before the first frame update
     void Start()
     {
